425409d4ce3e8af8718c19f770dc7f19e494ccd0
ManishV5 <bhagat.manishV5@gmail.com>
2022-09-15 12:10:46 +0530

    implemented jwt token

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('./node_modules/express')
 const app = express()
-const port = '8080'
+require('dotenv').config()
+const port = process.env.PORT
 
 const userServices = require('./services/users')
 const bodyParsar = require('./node_modules/body-parser')
 const bcrypt = require('bcrypt')
 
+const jwt = require('./node_modules/jsonwebtoken')
+const authMiddleware = require('./middleware/authenticateToken')
 
 app.get('/', (req, res) => {
     res.status(200).json({
@@ -24,8 +27,11 @@ app.post('/register', (req, res) => {
     const password = req.body.password
 
     id = userServices.addUser(username, mobile, password, email)
+    const token = {name: username}
+    const accessToken = jwt.sign(token, process.env.ACCESS_TOKEN_SECRET)
     res.status(200).json({
-        message: `User : ${username} inserted with id: ${id}`
+        message: `User : ${username} inserted with id: ${id}`,
+        accessToken: accessToken
     })
 })
 
@@ -36,8 +42,11 @@ app.post('/login', async (req, res) => {
     
     try{
         if(user[0].name === username && (await bcrypt.compare(password, user[0].password))){
+            const token = {name: username}
+            const accessToken = jwt.sign(token, process.env.ACCESS_TOKEN_SECRET)
             res.status(200).json({
-                message: 'Login successful'
+                message: 'Login successful',
+                accessToken: accessToken
             })
         } else {
             res.status(200).json({
@@ -51,7 +60,7 @@ app.post('/login', async (req, res) => {
     }    
 })
 
-app.get('/info/own/:username', (req, res) => {
+app.get('/info/own/:username', authMiddleware.authenticateToken, (req, res) => {
     const username = req.params.username
     const user = userServices.getUserByUsername(username)
     res.status(200).json({
@@ -59,7 +68,7 @@ app.get('/info/own/:username', (req, res) => {
     })
 })
 
-app.get('/info/other/:username', (req, res) => {
+app.get('/info/other/:username', authMiddleware.authenticateToken, (req, res) => {
     const username = req.params.username
     const users = userServices.getUsersOtherThan(username)
     res.status(200).json({
@@ -67,7 +76,7 @@ app.get('/info/other/:username', (req, res) => {
     })
 })
 
-app.post('/change/userinfo', (req, res) => {
+app.post('/change/userinfo', authMiddleware.authenticateToken, (req, res) => {
     const oldUsername = req.body.oldUsername
     const newUsername = req.body.newUsername
     const email = req.body.email
@@ -80,7 +89,7 @@ app.post('/change/userinfo', (req, res) => {
 })
 
 
-app.post('/change/password', (req, res) =>{
+app.post('/change/password', authMiddleware.authenticateToken, (req, res) =>{
     const username = req.body.username
     const newPassword = req.body.newPassword
     userServices.changePassword(username, newPassword)
---
+++ middleware/authenticateToken.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken")
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token_value = authHeader && authHeader.split(' ')[1]
+    if(token_value == null) return res.status(400).json({
+        message: 'no token found'
+    })
+
+    jwt.verify(token_value, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()  
+    })
+}
+
+module.exports = {
+    authenticateToken
+}
